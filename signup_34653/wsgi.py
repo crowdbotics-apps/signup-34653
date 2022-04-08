b3ea22e8f48f599619c27e19cc1965f8aa72f47e
Crowdbotics <team@crowdbotics.com>
2022-04-08 09:17:46 +0000

    Update models

--- signup_34653/wsgi.py
+++ signup_34653/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signup_34653.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "signup_34653.settings")
 
 application = get_wsgi_application()
